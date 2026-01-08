6e0278d3ac2ca3b96ff1d1cdf73b12b6bb6eac3c
HakuryuTB <jonathan.delvosal@edu.technobel.be>
2026-01-08 16:20:25 +0100

    fin de journée 08-01-26

--- EchoesOfTheRealmsAPI/Controllers/MonsterController.cs
+++ EchoesOfTheRealmsAPI/Controllers/MonsterController.cs
@@ -1,6 +1,9 @@
-﻿using EchoesOfTheRealmsShared.Services;
+﻿using EchoesOfTheRealms;
+using EchoesOfTheRealmsShared.Entities.MonsterFiles;
+using EchoesOfTheRealmsShared.Services;
 using EotR.App.Services;
 using EotRAPI.DTO;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -8,29 +11,38 @@ namespace EotRAPI.Controllers
 {
     [ApiController]
     [Route("api/monster")]
-    public class MonsterController(ClasseTest _classTest) : Controller
+    public class MonsterController(ClasseTest _classTest, MonsterService _mService) : ControllerBase
     {
+
         [HttpGet("{id}")]
         [ProducesResponseType(typeof(MonsterScreenResponseDTO), 200, "application/json")]
         [EndpointDescription("Permet d'afficher les statistiques d'un monstre, selon son ID / Allows you to display a monster's statistics, according to its ID")]
         public IActionResult MonsterScreen([FromRoute] int id)
         {
-
-            if (id == 1)
+            if (true) //si l'id est dans la db.... ?
             {
-                return Ok(new MonsterScreenResponseDTO { ID = id, Type = "", Name = "Loup des neiges" });
-            }
+                _mService.GetById(id);
 
-            else if (id == 2)
-            {
-                return Ok(new MonsterScreenResponseDTO { ID = id, Name = "Loup des pairies" });
+                return Ok(new MonsterScreenResponseDTO { id });
             }
 
             else return NotFound();
 
+            //if (id == 1)
+            //{
+            //    return Ok(new MonsterScreenResponseDTO { ID = id, Type = "", Name = "Loup des neiges" });
+            //}
+
+            //else if (id == 2)
+            //{
+            //    return Ok(new MonsterScreenResponseDTO { ID = id, Name = "Loup des pairies" });
+            //}
+
+
 
 
         }
+
 
     }
 
