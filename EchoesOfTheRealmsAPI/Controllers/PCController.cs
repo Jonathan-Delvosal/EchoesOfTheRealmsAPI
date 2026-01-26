58b8374cd681b6e251ccd46848f7b84b4636bb96
HakuryuTB <jonathan.delvosal@edu.technobel.be>
2026-01-26 16:26:13 +0100

    fin de journée 26-01-26 maj db équipement perso, début PCController et PCService

--- EchoesOfTheRealmsAPI/Controllers/PCController.cs
+++ EchoesOfTheRealmsAPI/Controllers/PCController.cs
@@ -1,19 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EchoesOfTheRealmsShared.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EchoesOfTheRealmsAPI.Controllers
 {
-    public class PCController : ControllerBase
+    [ApiController]
+    [Route("api/Character")]
+
+    public class PCController(PCService pCService) : ControllerBase
     {
 
-        // Todo: Get pour afficher les personnages crées par un utilisateur
-        // Todo: Post pour créer un personnage
+        //Todo: Get pour afficher les personnages crées par un utilisateur
+
+        [HttpGet]
+        [EndpointDescription("Récupère les personnages liés à un utilisateur")]
+
+
+        //Todo: Post pour créer un personnage
+        [HttpPost]
+        [EndpointDescription("Permet de créer un nouveau personnage lié à un utilisateur")]
 
         //Todo: Get pour stat perso ( a voir s'il faut pas changer la DB pour faire une classe avatar qui reprends les stats du perso + stat equip + stat job ... )
 
 
         //Todo: Get pour afficher les stat du perso
+        [HttpGet]
+        [EndpointDescription("Récupère les infos du personnage")]
+
+
         //Todo : Post pour modifier les stat du perso ( genre quand il up de lvl )
-        // Todo: Get pour afficher l'inventaire du perso
+        [HttpPost]
+        [EndpointDescription("Permet de modifier les stats d'un perso dans la DB")]
+
+
+        //Todo: Get pour afficher l'inventaire du perso
         //Todo; Get pour afficher les items equipés par le perso
 
         //Todo : get pour afficher les items pour changement d'equipement ( genre quand on veut equiper une arme, on voit les armes dans l'inventaire )
@@ -21,8 +40,6 @@ namespace EchoesOfTheRealmsAPI.Controllers
 
         //Todo: Update pour reposer le personnage ( genre quand il dort au campement )
 
-
-
-
     }
+
 }
