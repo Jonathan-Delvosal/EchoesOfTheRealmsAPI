fc4977923cb7bc0d821213767bb5df7de1d3315c
HakuryuTB <jonathan.delvosal@edu.technobel.be>
2026-02-19 15:15:55 +0100

    19-02-26 début combat API

---
+++ EchoesOfTheRealmsAPI/Controllers/Combat/CombatController.cs
@@ -0,0 +1,26 @@
+﻿using EchoesOfTheRealmsShared.DTO.Attack;
+using EchoesOfTheRealmsShared.Services.AttackService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EchoesOfTheRealmsAPI.Controllers.Combat
+{
+    [ApiController]
+    [Route("api/Combat")]
+    public class CombatController(CombatService _combatService) : ControllerBase
+    {
+        [HttpPost("Attack")]
+        [Authorize]
+        public ActionResult<AttackResultDTO> Attack([FromBody] AttackRequestDTO req)
+        {
+            if (req == null || req.Attacker == null || req.Defender == null)
+                return BadRequest();
+
+            if (req.AttackId <= 0)
+                return BadRequest();
+
+            var result = _combatService.ResolveAttack(req);
+            return Ok(result);
+        }
+    }
+}
